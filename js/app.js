faff97bf3d0805c091855bb51f36f3297f367c98
Sina Hosseini <sina.hosseini.gst@gmail.com>
2023-07-21 17:22:29 +0430

    Add  to Git

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // elements-1
+const textOne= document.getElementById('text-one');
 const fontFamilyOne= document.getElementById('font-family-one');
 const directionOne= document.getElementById('direction-one');
 const fontSizeOne= document.getElementById('font-size-one');
@@ -7,10 +8,12 @@ const lineHeightOne= document.getElementById('line-height-one');
 const letterSpacingOne= document.getElementById('letter-spacing-one');
 const wordSpacingOne= document.getElementById('word-spacing-one');
 const textAlignOne= document.getElementById('text-align-one');
-const textOne= document.getElementById('text-one');
 const colorOne= document.getElementById('color-one');
 const textShadowOne= document.getElementById('text-shadow-one');
+const fontStyleOne= document.getElementById('font-style-one');
+
 // elements-2
+const textTwo= document.getElementById('text-two');
 const fontFamilyTwo= document.getElementById('font-family-two');
 const directionTwo= document.getElementById('direction-two');
 const fontSizeTwo= document.getElementById('font-size-two');
@@ -19,10 +22,14 @@ const lineHeightTwo= document.getElementById('line-height-two');
 const letterSpacingTwo= document.getElementById('letter-spacing-two');
 const wordSpacingTwo= document.getElementById('word-spacing-two');
 const textAlignTwo= document.getElementById('text-align-two');
-const textTwo= document.getElementById('text-two');
 const colorTwo= document.getElementById('color-two');
 const textShadowTwo= document.getElementById('text-shadow-two');
+const fontStyleTwo= document.getElementById('font-style-two');
 
+// textarea
+const textarea= document.getElementsByTagName('textarea');
+
+// set up typography
 // section-1
 directionOne.addEventListener('keyup', () => {
   textOne.style.direction= directionOne.value;
@@ -70,6 +77,10 @@ colorOne.addEventListener('change', (event) => {
 
 textShadowOne.addEventListener('keyup', () => {
   textOne.style.textShadow= textShadowOne.value;
+});
+
+fontStyleOne.addEventListener('keyup', () => {
+  textOne.style.fontStyle= fontStyleOne.value;
 });
 
 // section-2
@@ -120,3 +131,19 @@ colorTwo.addEventListener('change', (event) => {
 textShadowTwo.addEventListener('keyup', () => {
   textTwo.style.textShadow= textShadowTwo.value;
 });
+
+fontStyleTwo.addEventListener('keyup', () => {
+  textTwo.style.fontStyle= fontStyleTwo.value;
+});
+
+// set up textarea
+function setHeight(val){
+  for(let j=0; j<textarea.length; j++){
+    textarea[j].style.height= val;
+  }
+}
+for(let i=0; i<textarea.length; i++){
+  textarea[i].addEventListener('mousemove', () => {
+    setHeight(window.getComputedStyle(textarea[i]).getPropertyValue('height'));
+  });
+}
